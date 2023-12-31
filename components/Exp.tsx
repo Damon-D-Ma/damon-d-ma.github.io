a55f0a0378a6135558c523e8523b0b8c08618bb9
Damon Ma <damondma@gmail.com>
2023-12-31 18:06:47 -0500

    Changed apostrophe to html equivalent

--- components/Exp.tsx
+++ components/Exp.tsx
@@ -24,7 +24,7 @@ export default function Exp() {
                                     <li>Contributed to the Social Needs Marketplace - Impact (SNM-I) project, a digital matchmaking web application that assists agencies in effectively providing social services to clients in need</li>
                                     <li>Successfully resolved 7+ critical bugs in the frontend application, ensuring application reliability and data integrity</li>
                                     <li>Collaborated with members of the development team to implement a shareability feature that allows agencies to share available programs and services with their designated partner organizations</li>
-                                    <li>Revamped the project's documentation system, providing a comprehensive guide to project setup, troubleshooting, and client-server architecture</li>
+                                    <li>Revamped the project&apos;s documentation system, providing a comprehensive guide to project setup, troubleshooting, and client-server architecture</li>
 
                         </ul>
                             </div>
